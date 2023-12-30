f20fa657bf1e2b42f8eb703064b345162f4e0a83
Brendan Landis <b@slownames.net>
2023-12-30 09:07:09 -0500

    formatting for Live page on mobile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,8 +18,8 @@ export default function About() {
                 </p>
                 <ul>
                     <li>
-                        <strong>art music & long-form instrumentals</strong> (new age doom, electroacoustic composition, free improvisation,
-                        prepared guitar, plunderphonics)
+                        <strong>art music & long-form instrumentals</strong> (new age doom, electroacoustic composition,
+                        free improvisation, prepared guitar, plunderphonics)
                     </li>
                     <li>
                         <strong>heavy music & non-music</strong> (harsh noise, industrial, grindcore)
@@ -35,29 +35,31 @@ export default function About() {
                     of making space for hope and horror in equal measure.
                 </p>
                 <p>
-                    After forty five releases, the Hey Exit project is retired for the foreseeable future. However,
-                    Brendan remains active in a number of other bands and projects:
+                    After forty five releases and one or two hundred shows, the Hey Exit project is retired for the
+                    foreseeable future. However, Brendan remains active in a number of other bands and projects:
                 </p>
                 <ul>
                     <li>
-                        <Link href="https://receive.blue/">Receive</Link> is his current primary creative output &mdash; a heavy art rock quartet
-                        in which he sings and plays baritone guitar.
+                        <Link href="https://receive.blue/">Receive</Link> is his current primary creative output &mdash;
+                        a heavy art rock quartet in which he sings and plays baritone guitar.
                     </li>
                     <li>
-                        <Link href="https://www.instagram.com/_starcard">Star Card</Link> is an alt rock band in which all members of Receive play different
-                        instruments. Brendan plays drums (hell yeah).
+                        <Link href="https://www.instagram.com/_starcard">Star Card</Link> is an alt rock band in which
+                        all members of Receive play different instruments. Brendan plays drums (hell yeah).
                     </li>
                     <li>
-                        <Link href="https://www.instagram.com/__receiver/">Receiver</Link> is a downer-pop-slash-noise-table duo involving Brendan and Calley
-                        Nelson (who sings in Star Card and plays lead guitar in Receive).
+                        <Link href="https://www.instagram.com/__receiver/">Receiver</Link> is a
+                        downer-pop-slash-noise-table duo involving Brendan and Calley Nelson (who sings in Star Card and
+                        plays lead guitar in Receive).
                     </li>
                     <li>
-                        <Link href="https://slownames.bandcamp.com/album/stairs-ep">Advection</Link> is Brendan's solo project for electronic music and short-form
-                        instrumentals.
+                        <Link href="https://slownames.bandcamp.com/album/stairs-ep">Advection</Link> is Brendan's solo
+                        project for electronic music and short-form instrumentals.
                     </li>
                     <li>
-                        <Link href="https://slownames.bandcamp.com/album/deluge">Anura</Link> is a bedroom songwriting project that preceded Receive by a year or two.
-                        It sometimes threatens to resurface in one form or another.
+                        <Link href="https://slownames.bandcamp.com/album/deluge">Anura</Link> is a bedroom songwriting
+                        project that preceded Receive by a year or two. It sometimes threatens to resurface in one form
+                        or another.
                     </li>
                 </ul>
             </main>
