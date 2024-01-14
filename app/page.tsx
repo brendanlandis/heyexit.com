60ce689df27bc8cf0137b9ff8beb21599ee6fe7b
Brendan Landis <b@slownames.net>
2024-01-14 16:14:47 -0500

    spot fix for links on home page

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                     <p>
                         This site is an archive for the now-defunct Hey Exit project, plus some related projects from
                         around the same time. You can <Link href="/about">read about it</Link>,{' '}
-                        <Link href="/discography">listen to recordings</Link>,
+                        <Link href="/discography">listen to it</Link>,{' '}
                         <Link href="/live">scroll through the gigography</Link>,{' '}
                         <Link href="/press">read a few reviews</Link>, or <Link href="/contact">get in touch</Link>.
                     </p>
