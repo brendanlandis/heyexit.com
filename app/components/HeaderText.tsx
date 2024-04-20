c7910f047a2485c6d9476d647b5dc188f0f9f8c6
Brendan Landis <b@slownames.net>
2024-04-20 12:01:35 -0400

    header typography is good enough

--- app/components/HeaderText.tsx
+++ app/components/HeaderText.tsx
@@ -1,15 +1,10 @@
 'use client';
 import React from 'react';
-// @ts-ignore
-// import FitText from '@kennethormandy/react-fittext';
 
 export default function HeaderText({ text, compressor }: { text: string; compressor: number }) {
     return (
         <>
-            <h1>
-                {text}
-                {/* <FitText compressor={compressor}>{text}</FitText> */}
-            </h1>
+            <h1>{text}</h1>
         </>
     );
 }
