1f0d127a2d19f95061cb0260bfc70b294f7bda3f
Brendan Landis <b@slownames.net>
2024-08-07 16:11:55 -0400

    changeup

---
+++ app/introText.tsx
@@ -0,0 +1,30 @@
+'use client';
+// @ts-ignore
+import Obfuscate from 'react-obfuscate';
+import Link from 'next/link';
+
+export default function IntroText() {
+  return (
+    <>
+      <p>
+        This site is an archive for the now-defunct Hey Exit project, plus some related bands and projects from around
+        the same time. Usually there's a lot of archival material to dig through here, but I'm moving the backend around
+        right now, so I expect this site to be down for about a month.
+      </p>
+      <p>
+        In the meantime, you can listen to the archives without any context on{' '}
+        <Link href="https://open.spotify.com/artist/59ul4nU3B016dyuv6XYn8e">Spewify</Link>, or with a little bit of
+        context on <Link href="https://slownames.bandcamp.com/music">Bandcamp</Link>.
+      </p>
+      <p>
+        These days, I've been playing in <Link href="https://receive.blue/">Receive</Link>,{' '}
+        <Link href="https://www.instagram.com/_starcard">Star Card</Link>, and{' '}
+        <Link href="https://www.instagram.com/__receiver/">Receiver</Link>.
+      </p>
+      <p>
+        ok, thanks for reading. Feel free to get in touch if you like: <Obfuscate email="b@slownames.net" />
+      </p>
+      <p>-Brendan, August 2024</p>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,26 @@
 import { Metadata } from 'next';
-import RandomLivePic from './RandomLivePic';
-import Link from 'next/link';
+// import RandomLivePic from './RandomLivePic';
+import IntroText from './introText';
 
 export const metadata: Metadata = {
-    title: 'Hey Exit',
+  title: 'Hey Exit',
 };
 
 export default function Home() {
-    return (
-        <>
-            <main id="home">
-                <div className="intro-text">
-                    <p>
-                        This site is an archive for the now-defunct Hey Exit project, plus some related projects from
-                        around the same time. You can <Link href="/about">read about it</Link>,{' '}
-                        <Link href="/discography">listen to it</Link>,{' '}
-                        <Link href="/live">scroll through the gigography</Link>,{' '}
-                        <Link href="/press">read a few reviews</Link>, or <Link href="/contact">get in touch</Link>.
-                    </p>
-                    <p>
-                        These days, Brendan plays in <Link href="https://receive.blue/">Receive</Link>,{' '}
-                        <Link href="https://www.instagram.com/_starcard">Star Card</Link>, and{' '}
-                        <Link href="https://www.instagram.com/__receiver/">Receiver</Link>.
-                    </p>
-                </div>
-                <RandomLivePic />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main id="home">
+        <div className="intro-text">
+          {/* <p>
+            This site is an archive for the now-defunct Hey Exit project, plus some related projects from around the
+            same time. You can <Link href="/about">read about it</Link>, <Link href="/discography">listen to it</Link>,{' '}
+            <Link href="/live">scroll through the gigography</Link>, <Link href="/press">read a few reviews</Link>, or{' '}
+            <Link href="/contact">get in touch</Link>.
+          </p> */}
+          <IntroText />
+        </div>
+        {/* <RandomLivePic /> */}
+      </main>
+    </>
+  );
 }
