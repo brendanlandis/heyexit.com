df143688388d84ddd6b9049040b90d9055db3e6b
Brendan Landis <b@slownames.net>
2024-11-07 15:16:53 -0500

    move pressList into component

---
+++ app/components/PressList.tsx
@@ -0,0 +1,91 @@
+'use client';
+import { Press } from '../types';
+import useAxios from 'axios-hooks';
+import { Fragment } from 'react';
+import Link from 'next/link';
+
+const shuffleArray = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export default function PressList() {
+  const [{ data: presses, loading, error }, refetch] = useAxios(
+    'https://slownames.net/api/presses?populate=bands&populate=recordings&populate=attachments&filters[visibility]=highlight&filters[bands][name]=Hey%20Exit&filters[type][$ne]=interview&pagination[pageSize]=100'
+  );
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>error</p>;
+
+  const GetPressDetails = (press: Press): Press => {
+    return {
+      id: press.id,
+      type: press.type,
+      date: press.date,
+      URL: press.URL,
+      attachments: press.attachments,
+      quote: press.quote,
+      visibility: press.visibility,
+      publication: press.publication,
+      bands: press.bands,
+      fullText: press.fullText,
+      recordings: press.recordings,
+    };
+  };
+  const formatPresses = (presses: { data: Press[] }): Press[] => {
+    return presses.data.map(GetPressDetails);
+  };
+
+  const formattedPresses = presses ? formatPresses(presses) : [];
+  const shuffledPresses = shuffleArray(formattedPresses);
+
+  return (
+    <ul className="pressList">
+      {shuffledPresses.map((press: Press) => (
+        <li key={press.id}>
+          {press.quote && <div className="press-quote">{`"${press.quote}"`}</div>}
+          <div className="press-credit">
+            {press.URL && (
+              <>
+                &mdash;
+                <Link href={press.URL} className="press-publication">
+                  {press.publication}
+                </Link>{' '}
+                on{' '}
+                <Link href="https://heyexit.com/">
+                  <em>{press.recordings[0].title}</em>
+                </Link>
+              </>
+            )}
+            {!press.URL && press.attachments && (
+              <>
+                {press.attachments.map((attachment) => (
+                  <Fragment key={attachment.id}>
+                    &mdash;
+                    <Link key={attachment.id} href={attachment.url} className="press-publication">
+                      {press.publication}
+                    </Link>{' '}
+                    on{' '}
+                    <Link href="https://heyexit.com/">
+                      <em>{press.recordings[0].title}</em>
+                    </Link>
+                  </Fragment>
+                ))}
+              </>
+            )}
+            {!press.URL && !press.attachments && (
+              <>
+                <span className="press-publication">&mdash;{press.publication}</span> on{' '}
+                <Link href="https://heyexit.com/">
+                  <em>{press.recordings[0].title}</em>
+                </Link>
+              </>
+            )}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,10 @@
 'use client';
 import './css/home.css';
 import Image from 'next/image';
-import { Press } from './types';
-import { GetPressDetails } from './components/GetPressDetails';
-import useAxios from 'axios-hooks';
-import { Fragment } from 'react';
-import Link from 'next/link';
 import Obfuscate from 'react-obfuscate';
-
-const shuffleArray = (array: any[]) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
+import PressList from './components/PressList';
 
 export default function Home() {
-  const [{ data: presses, loading, error }, refetch] = useAxios(
-    'https://slownames.net/api/presses?populate=bands&populate=recordings&populate=attachments&filters[visibility]=highlight&filters[bands][name]=Hey%20Exit&filters[type][$ne]=interview&pagination[pageSize]=100'
-  );
-  if (loading) return <p>loading...</p>;
-  if (error) return <p>error</p>;
-
-  const formatPresses = (presses: { data: Press[] }): Press[] => {
-    return presses.data.map(GetPressDetails);
-  };
-
-  const formattedPresses = presses ? formatPresses(presses) : [];
-  const shuffledPresses = shuffleArray(formattedPresses);
 
   return (
     <>
@@ -77,51 +53,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <ul className="pressList">
-          {shuffledPresses.map((press: Press) => (
-            <li key={press.id}>
-              {press.quote && <div className="press-quote">{`"${press.quote}"`}</div>}
-              <div className="press-credit">
-                {press.URL && (
-                  <>
-                    &mdash;
-                    <Link href={press.URL} className="press-publication">
-                      {press.publication}
-                    </Link>{' '}
-                    on{' '}
-                    <Link href="https://heyexit.com/">
-                      <em>{press.recordings[0].title}</em>
-                    </Link>
-                  </>
-                )}
-                {!press.URL && press.attachments && (
-                  <>
-                    {press.attachments.map((attachment) => (
-                      <Fragment key={attachment.id}>
-                        &mdash;
-                        <Link key={attachment.id} href={attachment.url} className="press-publication">
-                          {press.publication}
-                        </Link>{' '}
-                        on{' '}
-                        <Link href="https://heyexit.com/">
-                          <em>{press.recordings[0].title}</em>
-                        </Link>
-                      </Fragment>
-                    ))}
-                  </>
-                )}
-                {!press.URL && !press.attachments && (
-                  <>
-                    <span className="press-publication">&mdash;{press.publication}</span> on{' '}
-                    <Link href="https://heyexit.com/">
-                      <em>{press.recordings[0].title}</em>
-                    </Link>
-                  </>
-                )}
-              </div>
-            </li>
-          ))}
-        </ul>
+        <PressList />
       </div>
     </>
   );
