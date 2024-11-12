c20ea9e59250f980b586fa0657d4f6e7f545e6fe
Brendan Landis <b@slownames.net>
2024-11-12 09:48:49 -0500

    recording page is getting documentId

--- app/recordings/[normalizedTitle]/page.tsx
+++ app/recordings/[normalizedTitle]/page.tsx
@@ -1,3 +1,31 @@
-export default function RecordingPage({ params }: { params: { normalizedTitle: string } }) {
-  return <p>{params.normalizedTitle}</p>;
+import axios from 'axios';
+import getNormalizedTitle from '@/app/components/getNormalizedTitle';
+import { RecordingSummary } from '@/app/types';
+
+export default async function RecordingPage({ params }: { params: { normalizedTitle: string } }) {
+  try {
+    // fetch all recordings
+    const response = await axios.get(
+      'https://slownames.net/api/recordings?populate=*&filters[bands][name]=Hey%20Exit&filters[visibility][$ne]=hidden&pagination[pageSize]=100'
+    );
+    const recordings: RecordingSummary[] = response.data.data;
+
+    // find the one that matches
+    const matchingRecording = recordings.find((recording) => {
+      return getNormalizedTitle(recording.title) === params.normalizedTitle;
+    });
+
+    if (!matchingRecording) {
+      return <p>recording not found</p>;
+    }
+
+    // anyway, display it
+    return (
+      <>
+        <div>{matchingRecording.documentId}</div>
+      </>
+    );
+  } catch (error: any) {
+    return <p>error: {error.message}</p>;
+  }
 }
