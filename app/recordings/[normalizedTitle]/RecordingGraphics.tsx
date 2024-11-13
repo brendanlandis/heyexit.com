94152208a44b43e4f770c92543c104efd63599a0
Brendan Landis <b@slownames.net>
2024-11-13 11:34:04 -0500

    carousel is working!

--- app/recordings/[normalizedTitle]/RecordingGraphics.tsx
+++ app/recordings/[normalizedTitle]/RecordingGraphics.tsx
@@ -1,8 +1,19 @@
-import { Recording, Media, Edition } from '@/app/types';
+import { Recording } from '@/app/types';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useRef } from 'react';
 
 export default function RecordingGraphics(recording: Recording) {
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  function scrollCarousel(targetImageNumber: number) {
+    const carouselElement = carouselRef.current;
+    if (carouselElement) {
+      let carouselWidth = carouselElement.clientWidth;
+      let targetImage = targetImageNumber - 1;
+      let targetXPixel = carouselWidth * targetImage + 1;
+      carouselElement.scrollTo(targetXPixel, 0);
+    }
+  }
 
   // concatenate cover art, liner notes, pics of physical editions
   const getGraphics = () => {
@@ -14,9 +25,11 @@ export default function RecordingGraphics(recording: Recording) {
   };
   const graphicUrls = getGraphics();
 
+  let carouselElement = document.getElementById('carouselWrapper');
+
   return (
     <>
-      <div className="carousel w-full">
+      <div ref={carouselRef} className="carousel w-full">
         {graphicUrls.map((url, index) => {
           const prevIndex = index === 0 ? graphicUrls.length - 1 : index - 1;
           const nextIndex = index === graphicUrls.length - 1 ? 0 : index + 1;
@@ -24,12 +37,12 @@ export default function RecordingGraphics(recording: Recording) {
             <div id={`item${index + 1}`} className="carousel-item relative w-full" key={index}>
               <Image src={url} alt={`item number ${index + 1}`} width={750} height={750} className="w-full cover" />
               <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <Link href={`#item${prevIndex + 1}`} className="btn btn-circle">
+                <button onClick={() => scrollCarousel(prevIndex + 1)} className="btn btn-circle">
                   ❮
-                </Link>
-                <Link href={`#item${nextIndex + 1}`} className="btn btn-circle">
+                </button>
+                <button onClick={() => scrollCarousel(nextIndex + 1)} className="btn btn-circle">
                   ❯
-                </Link>
+                </button>
               </div>
             </div>
           );
@@ -37,9 +50,9 @@ export default function RecordingGraphics(recording: Recording) {
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
         {graphicUrls.map((_, index) => (
-          <Link href={`#item${index + 1}`} className="btn btn-xs" key={index}>
+          <button onClick={() => scrollCarousel(index + 1)} className="btn btn-xs" key={index}>
             {index + 1}
-          </Link>
+          </button>
         ))}
       </div>
     </>
