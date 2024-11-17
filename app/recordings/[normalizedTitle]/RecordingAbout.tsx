0e004f9aee8deab02cd9ac52c1efcd27b0920881
Brendan Landis <b@slownames.net>
2024-11-16 19:41:31 -0600

    checking blocks for content

--- app/recordings/[normalizedTitle]/RecordingAbout.tsx
+++ app/recordings/[normalizedTitle]/RecordingAbout.tsx
@@ -2,7 +2,23 @@ import { Recording } from '@/app/types';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 
 export default function RecordingAbout(recording: Recording) {
+  const hasContent =
+    Array.isArray(recording.about) &&
+    recording.about.some((block) => {
+      if (!block || !Array.isArray(block.children)) return false;
+      return block.children.some((child) => {
+        if (!('text' in child) || typeof child.text !== 'string') return false;
+        const cleanedText = child.text.replace(/<br\s*\/?>/g, '').trim();
+        return cleanedText.length > 0;
+      });
+    });
   return (
-    <div className="recording-about">{recording.about ? <BlocksRenderer content={recording.about} /> : <></>}</div>
+    <>
+      {hasContent ? (
+        <div className="recording-about">
+          <BlocksRenderer content={recording.about} />
+        </div>
+      ) : null}
+    </>
   );
 }
--- app/recordings/[normalizedTitle]/RecordingCredits.tsx
+++ app/recordings/[normalizedTitle]/RecordingCredits.tsx
@@ -2,12 +2,26 @@ import { Recording } from '@/app/types';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 
 export default function RecordingCredits(recording: Recording) {
+  const hasContent =
+    Array.isArray(recording.credits) &&
+    recording.credits.some((block) => {
+      if (!block || !Array.isArray(block.children)) return false;
+      return block.children.some((child) => {
+        if (!('text' in child) || typeof child.text !== 'string') return false;
+        const cleanedText = child.text.replace(/<br\s*\/?>/g, '').trim();
+        return cleanedText.length > 0;
+      });
+    });
   return (
-    <div className="recording-credits">
-      <div className="header-container">
-        <h2>credits</h2>
-      </div>
-      {recording.credits ? <BlocksRenderer content={recording.credits} /> : <></>}
-    </div>
+    <>
+      {hasContent ? (
+        <div className="recording-credits">
+          <div className="header-container">
+            <h2>credits</h2>
+          </div>
+          <BlocksRenderer content={recording.credits} />
+        </div>
+      ) : null}
+    </>
   );
 }
