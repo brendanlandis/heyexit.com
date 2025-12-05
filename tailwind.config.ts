8ed384af08e4ae2e1f762a78b00cc5f25b58c4f0
Brendan Landis <b@slownames.net>
2025-12-05 18:28:51 -0500

    ugh ok

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,17 +12,12 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      fontFamily: {
-        mono: ['var(--font-dm-mono)'],
-        circles: ['var(--font-charlottenburg-circles)'],
-        charlottenburg: ['var(--font-charlottenburg-classic)'],
-        lofi: ['var(--font-lofi-forest)'],
-      },
     },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ["black"],
-  },
+  plugins: [
+    require('daisyui')({
+      themes: ["black"],
+    })
+  ],
 };
 export default config;
